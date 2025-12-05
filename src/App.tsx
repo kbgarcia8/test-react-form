e22b811a29102603670e0b28f3a886eea817eb15
kbgarcia8 <kbgarcia8@gmail.com>
2025-12-06 01:42:11 +0800

    Dynamic Form Testing Part 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 //import AddressIcon from './assets/AddressIcon'
 import EditIcon from './assets/EditIcon'
@@ -118,26 +118,25 @@ const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
 const handleCancel = (e: React.MouseEvent<HTMLButtonElement>) => {
   const target = e.target as HTMLElement
   console.log(target)
-  alert('Cancel/Back button form clicked going back to previous page')
-}
-
-const handleReset = (e: React.MouseEvent<HTMLButtonElement>) => {
-  const target = e.target as HTMLElement
-  console.log(`${target.id} reset button form clicked`)
-}
-
-const handleAddOfEditableEntry = (e: React.MouseEvent<HTMLButtonElement>) => {
-  const target = e.target as HTMLElement
-  console.log(`${target.id} button for adding editable input clicked`)
+  if (confirm('Cancel/Back button form clicked going back to previous page')) {
+    window.location.reload()
+  }
 }
 
 function App() {
+
+  const { toggleTheme } = useTheme()
+  const initialized = React.useRef(false)
   
-  const handleEditClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+
+  const [fieldsetsValues, setFieldsetsValues] = React.useState<FieldsetShape[] | null>(null)
+  const [draftFieldsetValues, setDraftFieldSetValues] = React.useState<FieldsetShape[] | null>(null)
+  
+  const handleEditClick = React.useCallback((e:React.MouseEvent<HTMLButtonElement>) => {
     const target = e.currentTarget as HTMLElement
     const index = Number(target.dataset.index)
-    setDraftFieldSetValues((prevFieldset) =>
-      prevFieldset.map((fieldset)=> ({
+    setDraftFieldSetValues((prevDraftFieldset) => prevDraftFieldset &&
+      prevDraftFieldset.map((fieldset)=> ({
         ...fieldset,
         inputs: fieldset.inputs.map((input, idx) =>({
           ...input,
@@ -145,30 +144,33 @@ function App() {
         } as typeof input))
       }))
     )
-  }
+  },[])
 
-  const handleCancelClick = () => {
+  const handleCancelClick = React.useCallback(() => {
     
-    setDraftFieldSetValues(fieldsetsValues)
-  }
+    setDraftFieldSetValues(prevDraftFieldsetValues => fieldsetsValues)
+  }, [fieldsetsValues])
 
-  const handleDeleteClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+  const handleDeleteClick = React.useCallback((e:React.MouseEvent<HTMLButtonElement>) => {
     const target = e.currentTarget as HTMLElement
     const index = Number(target.dataset.index)
-    setDraftFieldSetValues((prevFieldsets) => (
-      prevFieldsets.map((fieldset) => ({
+    setDraftFieldSetValues((prevDraftFieldset) => {
+      if(!prevDraftFieldset) return prevDraftFieldset
+      const updated = prevDraftFieldset.map((fieldset) => ({
         ...fieldset,
         inputs: fieldset.inputs.filter((_, idx) => idx != index)
       }))
-    ))
-    setFieldsetsValues(draftFieldsetValues)
-  }
+      setFieldsetsValues(updated)
+      return updated
+    })
+    
+  }, [])
 
-  const handleLegendInputsOnChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const handleLegendInputsOnChange = React.useCallback((e:React.ChangeEvent<HTMLInputElement>) => {
     const target = e.currentTarget as HTMLElement
     const index = Number(target.dataset.index)
-    setDraftFieldSetValues((prevFieldset) =>
-      prevFieldset.map((fieldset)=> ({
+    setDraftFieldSetValues((prevDraftFieldset) => prevDraftFieldset &&
+      prevDraftFieldset.map((fieldset)=> ({ 
         ...fieldset,
         inputs: fieldset.inputs.map((input, idx) =>({
           ...input,
@@ -176,14 +178,14 @@ function App() {
         } as typeof input))
       }))
     )
-  }
+  }, [])
 
-  const handleChangeOfEditableInformation = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeOfEditableInformation = React.useCallback((e:React.ChangeEvent<HTMLInputElement>) => {
     const target = e.currentTarget
     const info = String(target.dataset.key)
     const editableIndex = Number(target.dataset.index)
     
-    setDraftFieldSetValues((prevDraftFieldset) =>
+    setDraftFieldSetValues((prevDraftFieldset) => prevDraftFieldset &&
       prevDraftFieldset.map((fieldset)=> ({
         ...fieldset,
         inputs: fieldset.inputs.map((input) =>({
@@ -197,52 +199,123 @@ function App() {
         } as typeof input))
       }))
     )
-  }
+  }, [])
 
-  const handleSaveClick = () => {
-    setDraftFieldSetValues((prevFieldset) =>
-      prevFieldset.map((fieldset)=> ({
+  const handleSaveClick = React.useCallback( async() => {
+    setDraftFieldSetValues((prevDraftFieldset) => {
+      if(!prevDraftFieldset) return prevDraftFieldset
+      const updated = prevDraftFieldset.map((fieldset)=> ({
         ...fieldset,
-        inputs: fieldset.inputs.map((input) =>({
+        inputs: fieldset.inputs.map((input, index) =>({
           ...input,
-          editing: false
+          editing: false,
+          id: `${input.id}-${index}`,
+          textLabel: `${input.editableInformation && input.editableInformation[0].info}`,
+          checked: false,
+          dataAttributes: {
+            "data-index": index,
+          },
         } as typeof input))
       }))
+      setFieldsetsValues(updated)
+      return updated
+  })
+  }, [])
+
+  const handleReset = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLElement
+    console.log(`${target.id} reset button form clicked`)
+    setDraftFieldSetValues((prevDraftFieldset) => prevDraftFieldset &&
+      prevDraftFieldset.map((fieldset)=> ({
+        ...fieldset,
+        inputs: []
+      }))
     )
-    setFieldsetsValues(draftFieldsetValues)
   }
 
-  const fieldsets:FieldsetShape[] = [
-    {
-          legend: "Address Informations",
-          inputs: addressInputsArray.map((address, index) => ({
-              ...address,
-              id: `${address.id}-${index}`,
-              textLabel: `${address.editableInformation[0].info}`,
-              checked: false,
-              isEditable: true as const,
-              onChange: handleLegendInputsOnChange,
-              onClickEdit: handleEditClick,
-              onClickDelete: handleDeleteClick,
-              onClickSave: handleSaveClick,
-              onClickCancel: handleCancelClick,
-              dataAttributes: {
-                  "data-index": index
-              }
-          })) as inputEntryShape<true, LabeledCheckboxOrRadio>[],
-          isExpandable: true
+  const handleAddOfEditableEntry = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLElement
+    console.log(`${target.id} button for adding editable input clicked`)
+    setDraftFieldSetValues((prevDraftFieldset) => prevDraftFieldset &&
+      prevDraftFieldset.map((fieldset)=> ({
+        ...fieldset,
+        inputs: [...fieldset.inputs,
+          {
+            //Input Props
+            type: "checkbox" as const,
+            id: "address-info",
+            isRequired: true,
+            //dataAttributes is obtained thru map
+            disabled: false,
+            name: "address",
+            checked: false,
+            //Start of Label Props
+            additionalInfo: `Additional address information ${fieldset.inputs.length + 1} of User`,
+            $labelFlexDirection: "row" as const,
+            //svg: <AddressIcon/>,
+            //Start of EditableInputProps
+            labelClass: "editable-label",
+            inputClass: "editable-input",
+            //Additional in inputEntryShape
+            isEditable: true as const,
+            editIcon: <EditIcon/>, //=>editIcon in EditableInputProps
+            deleteIcon: <DeleteIcon/>,
+            editing: true,
+            editableInformation: [ //These are informations editable within radio input acting as selection
+                {
+                    name: `Address ${fieldset.inputs.length + 1}`,
+                    info: '',
+                    type: 'text' as const
+                }
+            ],
+            onChange: handleLegendInputsOnChange,
+            onClickEdit: handleEditClick,
+            onClickDelete: handleDeleteClick,
+            onClickSave: handleSaveClick,
+            onClickCancel: handleCancelClick,
+          }
+        ]
+        }))
+    )
+  }
+  console.log('re-render')
+  //! Use for initialization only, not for remapping after save/delete
+  const fieldsets = React.useMemo<FieldsetShape[]>(() => {
+  return [
+      {
+        legend: "Address Informations",
+        inputs: addressInputsArray.map((address, index) => ({
+          ...address,
+          id: `${address.id}-${index}`,
+          textLabel: `${address.editableInformation[0].info}`,
+          checked: false,
+          isEditable: true as const,
+          onChange: handleLegendInputsOnChange,
+          onClickEdit: handleEditClick,
+          onClickDelete: handleDeleteClick,
+          onClickSave: handleSaveClick,
+          onClickCancel: handleCancelClick,
+          dataAttributes: {
+            "data-index": index,
+          },
+        })) as inputEntryShape<true, LabeledCheckboxOrRadio>[],
+        isExpandable: true,
       },
+    ];
+  }, [
+  handleLegendInputsOnChange,
+  handleEditClick,
+  handleDeleteClick,
+  handleCancelClick,
+  handleSaveClick]);
 
-  ];
-
-  const [fieldsetsValues, setFieldsetsValues] = useState<FieldsetShape[]>(fieldsets)
-  const [draftFieldsetValues, setDraftFieldSetValues] = useState<FieldsetShape[]>(fieldsetsValues)
-  console.log('saved')
-  console.dir(fieldsetsValues, { depth: null, colors: true });
-  
-  console.log('draft')
-  console.dir(draftFieldsetValues, { depth: null, colors: true });
-  const { toggleTheme } = useTheme()
+  React.useEffect(() =>{
+    if(!initialized.current) {
+      setFieldsetsValues(fieldsets)
+      setDraftFieldSetValues(fieldsets)
+      initialized.current = true
+    }
+  },[fieldsets])
 
   return (
     <div className='body-wrapper'>
@@ -254,7 +327,7 @@ function App() {
       <div className="with-fieldsets-container">
         <StyledDynamicForm
           className={'with-fieldsets'}
-          fieldsets={draftFieldsetValues}
+          fieldsets={draftFieldsetValues || []}
           id="address"
           isExpandable={true}
           inputClass={'address-field-input'}
